66000c48146f84f4927dfd0f06f52fb44e765954
Milesq <wmilosz88@gmail.com>
2020-07-24 17:38:28 +0200

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+// Jest supports TypeScript, only via Babel
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+}
