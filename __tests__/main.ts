a65147f26e3e966764f3b8e6ff85b290acc1ac08
Lumm1t <lummit1337@gmail.com>
2020-07-26 20:32:24 +0200

    Add basic unit test for login system

--- __tests__/main.ts
+++ __tests__/main.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from '../src/app'
 
-describe('login and register system', () => {
+describe('register system', () => {
   test('register works only on POST method', async () => {
     await request(app).get('/auth/register').expect(404)
   })
@@ -66,6 +66,39 @@ describe('login and register system', () => {
         },
       })
       .expect(201)
+
+    expect(response.body?.token).toBeInstanceOf(String)
+    expect(response.body?.token).toMatch(
+      /^[0-9a-z]+\.[0-9a-z]+\.[0-9a-z]+$/i
+    )
+  })
+})
+
+describe('login system', () => {
+  test("login fails when user data isn't provided or incomplete", async () => {
+    await request(app).post('/auth/login').expect(400)
+    await request(app)
+    .post('/auth/login')
+    .set('Content-Type', 'application/json')
+    .send({
+      user: {
+        name: 'John',
+      },
+    })
+    .expect(400)
+  })
+
+  test("login is succesfull when correct data is provided", async () => {
+    const response = await request(app)
+    .post('/auth/login')
+    .set('Content-Type', 'application/json')
+    .send({
+      user: {
+        name: 'John',
+        pass: 'zaq1@WSX',
+      },
+    })
+    .expect(200)
 
     expect(response.body?.token).toBeInstanceOf(String)
     expect(response.body?.token).toMatch(
