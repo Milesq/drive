1fd96f6216c4307d9fced50cafe31482cf08cc4f
Milesq <wmilosz88@gmail.com>
2020-07-27 13:14:38 +0200

    Connect with Mongo through Mongoose with .env file

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,8 @@ import { User } from './model';
 
 const router = Router();
 
-router.post('/auth/register', (req, res) => {
-  res.send('knaga papaja owns me and all');
+router.post('/register', (req, res) => {
+  res.send('Hello world');
 
   new User({
     userName: 'foo',
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
+import assert from 'assert';
+import mongoose from 'mongoose';
 import app from './app';
 
-app.listen(process.env.PORT || 8080);
+require('dotenv').config();
+
+['MONGO_URI'].forEach(variable => {
+  assert(process.env[variable], `process.env.${variable} is undefined!`);
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => console.log('Server is online'));
+  });
