8418921c5f9f85ff4a43bfe5d0d29844cac87f6f
Lumm1t <lummit1337@gmail.com>
2020-08-15 15:07:21 +0200

    Improve good practices

--- src/auth.ts
+++ src/auth.ts
@@ -9,15 +9,13 @@ router.post('/register', (req, res) => {
   const validPhoneNumber = /^(\+[0-9]{2})?[0-9]{3}[\s-]?[0-9]{3}[\s-]?[0-9]{3}$/i;
   const { name, pass, phone } = req.body.user;
 
-  if (!validPhoneNumber.test(phone)) {
-    return res.status(400).send('Invalid phone number!');
-  }
+  if (!validPhoneNumber.test(phone))
+    return res.status(400).send({ err: 'Invalid phone number!' });
 
-  if (name.length < 3) {
+  if (name.length < 3)
     return res.status(400).send({ err: 'Name is too short!' });
-  }
 
-  if (pass.length < 6) {
+  if (pass.length < 6)
     return res.status(400).send({ err: 'Password is too short!' });
   }
 });
