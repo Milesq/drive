1b0f96bdfd9d2171cd65d4a8a0b48049dd30a251
Lumm1t <lummit1337@gmail.com>
2020-08-15 14:46:37 +0200

    Separate auth tests to different files

--- __tests__/auth/register.ts
+++ __tests__/auth/register.ts
@@ -1,7 +1,7 @@
 import mockingoose from 'mockingoose'
 import request from 'supertest'
-import app from '../src/app'
-import { User } from '../src/model'
+import app from '../../src/app'
+import { User } from '../../src/model'
 
 describe('register system', () => {
   beforeEach(() => {
@@ -82,39 +82,6 @@ describe('register system', () => {
         },
       })
       .expect(201)
-
-    expect(response.body?.token).toBeInstanceOf(String)
-    expect(response.body?.token).toMatch(
-      /^[0-9a-z]+\.[0-9a-z]+\.[0-9a-z]+$/i
-    )
-  })
-})
-
-describe('login system', () => {
-  it("login fails when user data isn't provided or incomplete", async () => {
-    await request(app).post('/auth/login').expect(400)
-    await request(app)
-      .post('/auth/login')
-      .set('Content-Type', 'application/json')
-      .send({
-        user: {
-          name: 'John',
-        },
-      })
-      .expect(400)
-  })
-
-  it('login is succesfull when correct data is provided', async () => {
-    const response = await request(app)
-      .post('/auth/login')
-      .set('Content-Type', 'application/json')
-      .send({
-        user: {
-          name: 'John',
-          pass: 'zaq1@WSX',
-        },
-      })
-      .expect(200)
 
     expect(response.body?.token).toBeInstanceOf(String)
     expect(response.body?.token).toMatch(
