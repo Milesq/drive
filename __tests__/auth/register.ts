26aac9ac866ff79b66c544cf4fc430c411a035a3
Milesq <wmilosz88@gmail.com>
2020-08-16 16:32:09 +0200

    test: delete logic from unit tests

--- __tests__/auth/register.ts
+++ __tests__/auth/register.ts
@@ -3,6 +3,12 @@ import request from 'supertest'
 import app from '../../src/app'
 import { User } from '../../src/model'
 
+interface RegisterData {
+  name?: string
+  pass?: string
+  phone?: string
+}
+
 describe('register system', () => {
   beforeEach(() => {
     mockingoose.resetAll()
@@ -22,34 +28,36 @@ describe('register system', () => {
       .expect(400)
   })
 
-  it('register fails when user data is incorrect', async () => {
-    const incorrectData = [
-      { name: '$!`"' },
-      { pass: '' },
-      { phone: 123456789 },
-      { phone: '34' },
-      { phone: '123456789sadasd' },
-      { phone: 'sadasd' },
-    ]
+  describe('register fails when user data is incorrect', async () => {
+    function sendDamagedRequest(incorrect: RegisterData) {
+      return request(app)
+        .post('/auth/register')
+        .set('Content-Type', 'application/json')
+        .send({
+          user: {
+            name: 'John',
+            pass: 'zaq1@WSX',
+            phone: '123456789',
+            ...incorrect,
+          },
+        })
+        .expect(400)
+    }
 
-    const allExamples = incorrectData.map(
-      async incorrect => {
-        await request(app)
-          .post('/auth/register')
-          .set('Content-Type', 'application/json')
-          .send({
-            user: {
-              name: 'John',
-              pass: 'zaq1@WSX',
-              phone: '123456789',
-              ...incorrect,
-            },
-          })
-          .expect(400)
-      }
-    )
+    it("when name is'not alphanumeric", () =>
+      sendDamagedRequest({ name: '$!`"' }))
 
-    await Promise.all(allExamples)
+    it("when pass isn't passed", () =>
+      sendDamagedRequest({ pass: '' }))
+
+    it('when phone is too short', () =>
+      sendDamagedRequest({ phone: '34' }))
+
+    it('when phone is not number', () =>
+      sendDamagedRequest({ phone: '123456789sadasd' }))
+
+    it('when phone is not numeric', () =>
+      sendDamagedRequest({ phone: 'sadasd' }))
   })
 
   it('register fails when user already exists', async () => {
